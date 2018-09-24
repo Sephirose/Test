8748c4e93475e5b22d21329bba23caa3f8758f70
Administrator <Administrator@SC-201806070622>
2018-09-24 09:59:59 +0800

    out

--- src/test/mail.java
+++ src/test/mail.java
@@ -3,8 +3,8 @@ package test;
 public class mail {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		String update = "update from github";
+		System.out.println(update);
 	}
 
 }
